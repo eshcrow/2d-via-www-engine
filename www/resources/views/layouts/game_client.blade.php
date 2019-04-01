6233f3cb1cc8103d1a6e86945599e0b7d14870e5
Szymon Fox <trzebu@gmail.com>
2019-04-01 20:42:27 +0200

    Client html template

--- www/resources/views/layouts/game_client.blade.php
+++ www/resources/views/layouts/game_client.blade.php
@@ -1,1 +1,13 @@
-game client
+<html lang="pl">
+
+    <head>
+        <title>{{ env('APP_NAME') }}</title>
+        <link href="{{ url('game_client/src/scripts/css/game.css') }}" rel="stylesheet" type="text/css">
+        <script src="{{ url('game_client/src/scripts/js_libs/require.js') }}" data-main="{{ url('game_client/src/scripts/js/engine.js') }}"></script>
+    </head>
+
+    <body>
+
+    </body>
+
+</html>
